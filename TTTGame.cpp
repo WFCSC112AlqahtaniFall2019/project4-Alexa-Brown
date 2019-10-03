8a418cf8fbd5a086d295923f90ab310b4ffdca1b
Alexa-Brown <browam18@wfu.edu>
2019-10-03 16:44:14 -0400

    Adding a 1 player game

--- TTTGame.cpp
+++ TTTGame.cpp
@@ -5,6 +5,8 @@
 
 #include <iostream>
 #include "TicTacToe.h"
+#include <time.h>
+
 using namespace std;
 
 int main() {
@@ -15,23 +17,25 @@ int main() {
     char playAgain = 'y';
     //bool winnerYet;
 
-    while(playAgain = 'y'){
-    cout << " Welcome to TicTacToe! How many players would you like to play with? (1 or 2)"<< endl;
-    cin >> numPlayers;
+    while(playAgain != 'q') {
 
-    if (numPlayers != 1 && numPlayers != 2){
-        cout << "Please try again and enter either 1 or 2 players." <<endl;
-        return 0;
-    }
-
-    //if number of players is 2 (one is not the computer)
-    else if (numPlayers == 2) {
-        cout << "Here is the playing board: " << endl;
-        variable.printBoard();
+        variable.initializeBoard(); //to reset the board if the player wants to play again
+        cout << " Welcome to TicTacToe! How many players would you like to play with? (1 or 2)" << endl;
+        cin >> numPlayers;
 
 
-       // while (!variable.isBoardFull()) {
-            while (!variable.checkForWin()){//to cycle between players
+        if (numPlayers != 1 && numPlayers != 2) {
+            cout << "Please try again and enter either 1 or 2 players." << endl;
+            return 0;
+        }
+
+            //if number of players is 2 (two users)
+        else if (numPlayers == 2) {
+            cout << "Here is the playing board: " << endl;
+            variable.printBoard();
+
+
+            while (!variable.checkForWin()) {//to cycle between players
                 cout << "Player 1: you are 'x'" << endl;
                 cout << "Enter row and column (0, 1, 2)" << endl;
                 cin >> row;
@@ -39,7 +43,10 @@ int main() {
                 variable.placeMark(row, column);
                 variable.printBoard();
                 variable.changePlayer();
-                variable.checkForWin();
+                // variable.checkForWin();
+                if (variable.checkForWin()) {
+                    break;
+                }
 
                 cout << "Player 2: you are 'o'" << endl;
                 cout << "Enter row and column (0, 1, 2)" << endl;
@@ -48,18 +55,65 @@ int main() {
                 variable.placeMark(row, column);
                 variable.printBoard();
                 variable.changePlayer();
-               // variable.isBoardFull();
-                variable.checkForWin();
+                if (variable.isBoardFull())
+                    break;
+            }
+            if (variable.checkForWin()) {
+                break;
             }
             if (variable.checkForWin()) {
                 variable.changePlayer(); //since we switch players at the end of the while loop, we must switch back for the winner
-                cout << "You win Player " << variable.getCurrentPlayerNumber() << "!";
-                cout << "Do you want to play again? Input y to play again or any other key to quit." << endl;
+                cout << "You win Player " << variable.getCurrentPlayerNumber() << "!" << endl;
+                cout << "Do you want to play again? Input 'y' to play again or 'q' to quit." << endl;
                 cin >> playAgain;
             }
+        }//end of if players = 2
 
-        }
+
+            //one user is a player and the computer is a player
+        else if (numPlayers == 1) {
+            cout << "Here is the playing board: " << endl;
+            variable.printBoard();
+
+            while (!variable.checkForWin()) {//to cycle between players
+                cout << "User's turn: you are 'x'" << endl;
+                cout << "Enter row and column (0, 1, 2)" << endl;
+                cin >> row;
+                cin >> column;
+                variable.placeMark(row, column);
+                variable.printBoard();
+                variable.changePlayer();
+                // variable.checkForWin();
+                if (variable.checkForWin()) {
+                    break;
+                }
+
+                srand (time(NULL));
+
+                cout << "Computer's turn: you are 'o'" << endl;
+                row = rand()% 2 + 1;
+                column = rand()% 2 + 1;
+                variable.placeMark(row, column);
+                variable.printBoard();
+                variable.changePlayer();
+                if (variable.isBoardFull())
+                    break;
+            }
+            if (variable.checkForWin()) {
+                break;
+            }
+            if (variable.checkForWin()) {
+                variable.changePlayer(); //since we switch players at the end of the while loop, we must switch back for the winner
+                cout << "You win Player " << variable.getCurrentPlayerNumber() << "!" << endl;
+                cout << "Do you want to play again? Input 'y' to play again or 'q' to quit." << endl;
+                cin >> playAgain;
+            }
+        } //end of if players = 1
     }
+
+
+
+
 
 
     // FINISH ME
